04cb8c13568f32034f5d9b1ac730dc4c52e702f7
Vignesh R <rvignesh60474@gmail.com>
2024-10-20 13:15:58 +0530

    home page

---
+++ src/Components/Headers/Headers.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Headers.css'
+export const Headers = () => {
+  return (
+    <div className='headers'>
+        <button className='logout'>Logout</button>
+    </div>
+  )
+}
--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -1,25 +1,43 @@
 import React from 'react'
 import './home.css'
+import { Headers } from '../Headers/Headers';
 export const Home = () => {
   const names = [
-    { id: 'sdf', name: 'Ssdf' },
-    { id: 'sadf', name: 'Ssasdf' },
-    { id: 'sdsaf', name: 'Ssasadf' }
+    { id: '1', name: 'Ssdf' },
+    { id: '2', name: 'Ssasdf' },
+    { id: '3', name: 'Ssasadf' }
   ];
+
+  const messages=[
+    {message:"one",time:"Sdfsd",in:true},{message:"two",time:"sdfsdf",in:false},{message:"one",time:"Sdfsd",in:true},{message:"two",time:"sdfsdf",in:false},{message:"one",time:"Sdfsd",in:true},{message:"two",time:"sdfsdf",in:false},{message:"one",time:"Sdfsd",in:true},{message:"two",time:"sdfsdf",in:false}
+  ]
 const onclickfunction=(data)=>{
-  console.log("ssssssss",data.target)
+  console.log(data)
 }
   return (
     <>
     <div>
-      <ul style={{ listStyleType: 'none' }}>
+<Headers/>
+      <div className='sideBox'>
+
+      <ul className='table' style={{ listStyleType: 'none' }}>
 
         {names.map((name)=>(
-          <li className='names' onClick={onclickfunction} key={name.id} >{name.name}</li>
+          <li className='names' onClick={()=>{onclickfunction(name.id)}} key={name.id} >{name.name}</li>
+
         ))}
 
       </ul>
     </div>
+    <div className='TextBox'>
+{messages.map((message)=>(
+<div key={message.time} className={message.in?'message':"messagesent"}>{message.message}</div>
+))}
+    </div>
+    <div>
+
+    </div>
+        </div>
 
     </>
   )
